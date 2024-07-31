65aa8c29c2ce59aecde4a9c5a111318f2d1570e6
Yangyuming <2822758820@qq.com>
2024-07-31 14:15:02 +0800

    RCNN

---
+++ src/main/java/com/niit/rnnface/RnnFaceApplication.java
@@ -0,0 +1,13 @@
+package com.niit.rnnface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnnFaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RnnFaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niit/rnnface/RnnFaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.rnnface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnnFaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
